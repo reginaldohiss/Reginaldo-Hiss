cb8833507d1d2196caa5534cb6f990456fd9b6a4
reginaldohiss <reginaldosilvas2001@gmail.com>
2020-08-10 03:03:08 -0300

    Scroll Personalizado Inserido

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ $(function() {
         e.preventDefault();
         var id = $(this).attr("href");
         var targetOffset = $(id).offset().top;
-        var menuHeight = $(".menu_nav").innerHeight();
 
-        // console.log(targetOffset);
         $("html, body").animate({
             scrollTop: targetOffset
-        }, 700);    
+        }, 750);    
    });
 });
